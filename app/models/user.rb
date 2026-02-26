e3acd8b52836900ec1665d4276225e7763de6794
Ghee-clarified-butter <gmedeiro@andrew.cmu.edu>
2026-02-25 19:38:25 -0500

    Whatever you write on this line will be the commit message.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
-  # validates :role, presence: true, inclusion: { in: [0, 1], message: "must be either 0 (admin) or 1 (regular user)" }
-  # validates :active, inclusion: { in: [true, false], message: "must be true or false" }
-
-  # enum role: { admin: 0, regular_user: 1 }
-
-  # scope :active_users, -> { where(active: true) }
-  # scope :inactive_users, -> { where(active: false) }
 
   def proper_name
     "#{first_name} #{last_name}"
--- config.ru
+++ config.ru
@@ -1,13 +1,5 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-resources :users
-resources :sessions
-get 'user/edit' => 'users#edit', :as => :edit_current_user
-get 'signup' => 'users#new', :as => :signup
-get 'login' => 'sessions#new', :as => :login
-get 'logout' => 'sessions#destroy', :as => :logout
 
-# Default route
-root :to => 'bands#index', :as => :home
 run Rails.application
